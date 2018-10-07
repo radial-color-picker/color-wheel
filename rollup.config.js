611887cc0a8d6cc28d8cee32a76dd6d42f1f1395
Rosen Kanev <rkunev@gmail.com>
2018-10-07 16:17:09 +0300

    build: Replace butternut with babel-minify and add banner

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,20 @@
-import butternut from 'rollup-plugin-butternut';
+import minify from 'rollup-plugin-babel-minify';
 import babel from 'rollup-plugin-babel';
+import pkg from './package.json';
+
+const banner = `/**
+ * radial-color-picker/color-wheel v${pkg.version}
+ *
+ * https://github.com/radial-color-picker/color-wheel
+ *
+ * Copyright (c) 2018-present, Rosen Kanev
+ * Released under the MIT License.
+ */`;
 
 export default [
     {
         input: 'src/main.js',
-        output: { name: 'colorWheel', file: 'dist/color-wheel.umd.js', format: 'umd' },
+        output: { name: 'colorWheel', file: pkg.browser, format: 'umd', banner },
         plugins: [
             babel({
                 exclude: ['node_modules/**'],
@@ -13,24 +23,22 @@ export default [
     },
     {
         input: 'src/main.js',
-        output: { name: 'colorWheel', file: 'dist/color-wheel.umd.min.js', format: 'umd' },
+        output: { name: 'colorWheel', file: 'dist/color-wheel.umd.min.js', format: 'umd', banner },
         plugins: [
             babel({
                 exclude: ['node_modules/**'],
             }),
-            butternut()
+            minify({
+                comments: false,
+                bannerNewLine: true
+            })
         ]
     },
     {
         input: 'src/main.js',
         output: [
-            { file: 'dist/color-wheel.cjs.js', format: 'cjs' },
-            { file: 'dist/color-wheel.esm.js', format: 'es' }
-        ],
-        plugins: [
-            babel({
-                exclude: ['node_modules/**'],
-            })
+            { file: pkg.main, format: 'cjs', banner },
+            { file: pkg.module, format: 'es', banner }
         ]
     }
 ];
